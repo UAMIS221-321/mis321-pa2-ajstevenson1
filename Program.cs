35980dc7fb6334cadfdb930fa816f8438bde64a4
Andrew Stevenson <andrewstevenson@Andrews-MacBook-Pro-2.local>
2022-02-22 14:34:46 -0600

    created program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mis321_pa2_ajstevenson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
